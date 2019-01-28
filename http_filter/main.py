350f61854e542677e3e6ca2f71ae5117a721632c
shabirmean <shabir_tck09@hotmail.com>
2019-01-27 21:07:45 -0500

    Fixed the http filter VMF

--- http_filter/main.py
+++ http_filter/main.py
@@ -6,21 +6,23 @@ url = 'http://172.17.42.1:8081/notification'
 
 def callback(pkt):
         parsed = pkt.get_payload()
-        print parsed.encode('hex')
+        print "Message received:\n\t" + parsed.encode('hex')
 
         if 'hack' in parsed:
             pkt.drop()
-            print "packet dropped"
-            requests.post(url, data="Http Filtered content")	
+            print "packet dropped"  
+            #requests.post(url, data="Http Filtered content")	      
+            # Uncomment and set a right URL to send a notification
         else:
             pkt.accept()
-
+        print "-------------------------------------------"
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(1, callback)
 try:
+    print "Starting HTTP_Filter..."
     nfqueue.run()
 except KeyboardInterrupt:
-    print
+    print "An exceptipn occured"
 
 
