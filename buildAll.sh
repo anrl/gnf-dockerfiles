906ae6bec1013957441bdd9e99c7c876eb1d91d0
shabirmean <shabir_tck09@hotmail.com>
2019-01-27 20:44:58 -0500

    Added bashrc updates to the build script

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Building docker images....."
 CURR_FOLDER=`pwd`
 for folder in $(ls -d *)
 do
@@ -10,3 +11,14 @@ do
     fi
     cd $CURR_FOLDER
 done
+
+echo "-----------------------------------"
+echo "Setting .bashrc"
+
+GLANF_FOLDER="$CURR_FOLDER/testing"
+echo "export GLANF_HOME=\"$GLANF_FOLDER/\"" >> ~/.bashrc
+echo "export PATH=\"$PATH:$GLANF_HOME\"" >> ~/.bashrc
+echo "alias glanf_start=\"sudo $GLANF_HOME/glanf start\"" >> ~/.bashrc
+echo "alias glanf_stop=\"sudo $GLANF_HOME/glanf stop\"" >> ~/.bashrc
+echo "alias glanf_clean=\"sudo $GLANF_HOME/glanf clean\"" >> ~/.bashrc
+echo "alias glanf_reset=\"sudo $GLANF_HOME/glanf stop && sudo $GLANF_HOME/glanf clean\"" >> ~/.bashrc
